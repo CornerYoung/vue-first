88282f7e966c2cca83f2063fed2e4d75ed8f7c0b
ygn <947498533@qq.com>
2018-04-12 22:30:41 +0800

    Counter.vue组件

---
+++ ImoocMall/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <p><span>{{num}}</span></p>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+      return{
+          num:0
+      }
+  },
+  methods:{
+      increment(){
+          this.num++;
+      },
+      decrement(){
+          this.num--;
+      }
+  }
+}
+</script>
