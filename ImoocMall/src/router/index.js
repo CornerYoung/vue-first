27b45dd42f023171826bc6b598eb929ce73df5ea
ygn <947498533@qq.com>
2018-04-16 21:13:06 +0800

    动态路由的匹配

--- ImoocMall/src/router/index.js
+++ ImoocMall/src/router/index.js
@@ -3,11 +3,12 @@ import Router from 'vue-router'
 import GoodsList from './../views/GoodsList'
 
 Vue.use(Router)
-
+//Vue Router 就是对原生 History 的封装
 export default new Router({
+  mode:'history', //或者设置参数为 hash ，注意#的使用
   routes: [
-    {
-      path: '/',
+    {//动态路由，必须符合我的规则，必须是以goods开头的，而且后面必须紧跟一个动态的变量
+      path: '/goods/:goodsId/user/:admin',  
       name: 'GoodsList',
       component: GoodsList
     }
