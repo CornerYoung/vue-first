3f6f142f29b6c8a26e9adb7bd61dcf6f85d80357
ygn <947498533@qq.com>
2018-04-25 11:11:15 +0800

    将原来的views文件夹改为views-practice文件夹，views文件夹内放正式项目的组件

---
+++ ImoocMall/src/views-practice/Cart.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+      这是购物车页面
+       <span>{{$route.query.goodsId}}</span>  <!--参数传递 -->
+  </div>
+</template>
---
+++ ImoocMall/src/views-practice/GoodsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      这是商品列表页面
+      <span>{{$route.params.goodsId}}</span><br>
+      <span>{{$route.params.admin}}</span>
+      <router-link to="/goods/title">显示商品标题</router-link>
+      <router-link to="/goods/img">显示商品图品</router-link>
+      <div>
+        <router-view></router-view>
+      </div>
+      <router-link to="/cart">跳转到购物车页面</router-link>
+      <button @click="jump">button - 跳转到购物车页面</button>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  data(){
+      return {
+          msg: 'hello vue'
+      }
+  },
+  methods:{
+      jump(){
+          //this.$router.push('/Cart')
+          //this.$router.push({path:'/cart'})
+          this.$router.push({path:'/cart?goodsId=123'})
+          //this.$router.go(-2)
+      }
+  }
+}
+</script>
+
