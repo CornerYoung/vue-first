16f4ba1099dc9f0aee1545705431a88f49916552
ygn <947498533@qq.com>
2018-04-25 11:16:05 +0800

    views文件夹上位，确立为正式项目文件夹，原pages文件夹改名为views

--- ImoocMall/src/views/Cart.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-      这是购物车页面
-       <span>{{$route.query.goodsId}}</span>  <!--参数传递 -->
-  </div>
-</template>
--- ImoocMall/src/views/Title.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-      这里是商品标题的子组件
-  </div>
-</template>
