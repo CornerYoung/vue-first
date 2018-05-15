1f8f7005fda8fd7c062ac293853d1718ce9d69be
ygn <947498533@qq.com>
2018-05-15 21:48:47 +0800

    尝试运行node.js

--- ImoocMall/server/Demo.js
+++ ImoocMall/server/Demo.js
@@ -1,5 +1,5 @@
 let user = require('./User');
 
 console.log(`userName:${user.userName}`);
-user.sayHello();
-console.log(`I'am ${user.userName}`);
+
+console.log(`I'am ${user.userName},I say ${user.sayHello()}`);
--- ImoocMall/server/User.js
+++ ImoocMall/server/User.js
@@ -1,4 +1,4 @@
 module.exports = {
     userName:'杨国宁',
-    sayHello:()=>{console.log('hello!')}
+    sayHello:()=>{return 'Hello!'}
 }
