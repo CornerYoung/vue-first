b984fb03351aa8f60e6e01877f1d49b7e1d6204e
ygn <947498533@qq.com>
2018-05-18 22:17:11 +0800

    http.createServer返回一个对象，赋值给自己声明的server，在server上使用listen方法

--- ImoocMall/server/Demo.js
+++ ImoocMall/server/Demo.js
@@ -6,12 +6,12 @@ console.log(`I'am ${user.userName},I say ${user.sayHello()}`);
 
 let http = require('http');
 
-http.createServer((req,res)=>{
+let server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain;charset=utf-8');
     res.end('Hello,Node.js');
 });
 
-http.listen(3000,'127.0.0.1',function(){
+server.listen(3000,'127.0.0.1',function(){
     console.log('服务器已经运行，请打开浏览器，输入 http://127.0.0.1:3000 来进行访问！')
 });
