52834f03f53a39ae86594f900726446eb17e967e
ygn <947498533@qq.com>
2018-05-18 22:12:54 +0800

    http.server

--- ImoocMall/server/Demo.js
+++ ImoocMall/server/Demo.js
@@ -3,3 +3,15 @@ let user = require('./User');
 console.log(`userName:${user.userName}`);
 
 console.log(`I'am ${user.userName},I say ${user.sayHello()}`);
+
+let http = require('http');
+
+http.createServer((req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain;charset=utf-8');
+    res.end('Hello,Node.js');
+});
+
+http.listen(3000,'127.0.0.1',function(){
+    console.log('服务器已经运行，请打开浏览器，输入 http://127.0.0.1:3000 来进行访问！')
+});
