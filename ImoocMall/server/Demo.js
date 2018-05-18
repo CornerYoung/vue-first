dc361057f6c6d3c1f73b1329eda2e3e5606ff7c2
ygn <947498533@qq.com>
2018-05-18 22:25:09 +0800

    引入uil、util模块

--- ImoocMall/server/Demo.js
+++ ImoocMall/server/Demo.js
@@ -5,11 +5,15 @@ console.log(`userName:${user.userName}`);
 console.log(`I'am ${user.userName},I say ${user.sayHello()}`);
 
 let http = require('http');
+let url = require('url');
+let util = require('util');
 
 let server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain;charset=utf-8');
-    res.end('Hello,Node.js');
+    
+    
+    res.end(util.inspect(url.parse(req.url)));
 });
 
 server.listen(3000,'127.0.0.1',function(){
