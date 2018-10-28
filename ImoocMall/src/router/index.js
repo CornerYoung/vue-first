8bfb576c994b3fb319ed7246a6d60364eb84bb33
ygn <947498533@qq.com>
2018-10-28 20:34:03 +0800

    vue路由

--- ImoocMall/src/router/index.js
+++ ImoocMall/src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import GoodsList from './../views/GoodsList'
+import Home from '@/views/home/Home'
+import List from '@/views/list/List'
+//import HelloWorld from '../components/HelloWorld.vue'
+//import GoodsList from './../views/GoodsList'
 Vue.use(Router);
 export default new Router({
   mode:'history', //或者设置参数为 hash ，注意#的使用
   routes: [
     {//动态路由，必须符合我的规则，必须是以goods开头的，而且后面必须紧跟一个动态的变量
       path: '/',  
-      name: 'GoodsList',
-      component: GoodsList
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
