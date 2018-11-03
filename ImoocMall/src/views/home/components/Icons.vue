f5275b88d85b407db8beffe97896e79e6e7ea0f6
ygn <947498533@qq.com>
2018-11-03 16:20:23 +0800

    解决icon区域图标随同轮播图一起轮播滑动的bug

--- ImoocMall/src/views/home/components/Icons.vue
+++ ImoocMall/src/views/home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="icons">
-        <swiper>
+        <swiper :options="swiperOption">
             <swiper-slide v-for="(page,index) of pages" :key="index">
                 <div class="icon" v-for="item of pages[index]" :key="item.id">
                     <div class="icon-img">
