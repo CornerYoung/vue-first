b63d15152e114cafdc39145616ff85dfe165f2c5
ygn <947498533@qq.com>
2018-11-04 17:47:20 +0800

    修改git设置，本地模拟数据不提交仓库。在home主页引入axios实现ajax功能

--- ImoocMall/src/views/home/Home.vue
+++ ImoocMall/src/views/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper';
 import Icons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
     name: 'Home',
     components:{
@@ -22,6 +23,9 @@ export default {
         Icons,
         HomeRecommend,
         HomeWeekend
+    },
+    mounted () {
+        this.getHomeInfo()
     }
 }
 </script>
