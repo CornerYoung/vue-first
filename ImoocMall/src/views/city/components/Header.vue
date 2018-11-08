756bc659b5668664ce6b32b5dbe074131768c600
ygn <947498533@qq.com>
2018-11-08 21:37:48 +0800

    城市选择头部添加返回箭头，并实现点击返回首页功能

--- ImoocMall/src/views/city/components/Header.vue
+++ ImoocMall/src/views/city/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="header">
         城市选择
+        <router-link to="/">
+            <div class="header-back iconfont">&#xe624;</div>
+        </router-link>
     </div>
 </template>
 
@@ -13,6 +16,7 @@ export default {
 <style lang="stylus" scoped>
 @import '~css/varibles.styl'
 .header
+    position relative
     overflow hidden
     height 0.86rem
     line-height 0.86rem
@@ -20,4 +24,11 @@ export default {
     color #ffffff
     background-color $bgColor
     font-size 0.32rem
+    .header-back
+        width 0.64rem
+        font-size 0.4rem
+        text-align center
+        position absolute
+        top 0
+        left 0
 </style>
