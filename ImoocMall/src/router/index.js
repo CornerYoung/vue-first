4eb2a54920401ffd4fa1baeadcede0d29d8564f7
ygn <947498533@qq.com>
2018-11-08 21:12:28 +0800

    配置城市路由。在路由文件夹中引入城市组件，配置路径path: ”/city“

--- ImoocMall/src/router/index.js
+++ ImoocMall/src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home/Home'
+import City from '@/views/city/City'
 //import HelloWorld from '../components/HelloWorld.vue'
 //import GoodsList from './../views/GoodsList'
 Vue.use(Router);
 export default new Router({
   mode:'history', //或者设置参数为 hash ，注意#的使用
   routes: [
-    {//动态路由，必须符合我的规则，必须是以goods开头的，而且后面必须紧跟一个动态的变量
+    {//主页路由
       path: '/',  
       name: 'Home',
       component: Home
+    },
+    {//城市路由
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })
