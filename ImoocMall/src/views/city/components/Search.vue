b02ca84f8cedb736fb02e905932afd1a555f3677
ygn <947498533@qq.com>
2018-11-21 20:44:08 +0800

    城市搜索静态页面及样式。搜索框数据

--- ImoocMall/src/views/city/components/Search.vue
+++ ImoocMall/src/views/city/components/Search.vue
@@ -1,12 +1,33 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" name="" id="" placeholder="输入城市名或拼音">
+    <div class="search-wrapper">
+        <div class="search">
+            <input 
+                class="search-input" 
+                type="text" 
+                name="" 
+                id="" 
+                placeholder="输入城市名或拼音"
+                v-model="city"
+            >
+        </div>
+        <div class="search-detaile">
+            <ul>
+                <li class="city-item">{{city}}</li>
+                <li class="city-item">上海</li>
+                <li class="city-item">杭州</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    data () {
+        return {
+            city:''
+        }
+    }
 }
 </script>
 
@@ -25,5 +46,18 @@ export default {
         text-align center
         border-radius 0.06rem
         color #666
+.search-detaile
+    width 100%
+    height 100%
+    background-color #eee
+    position absolute
+    z-index 1
+    top 1.58rem
+    bottom 0
+    left 0
+    right 0
+    .city-item
+        line-height 0.5rem
+        padding-left 0.1rem
 </style>
 
