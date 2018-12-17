2e75c9aa2248161b742340e5d0332b0896d26c67
ygn <947498533@qq.com>
2018-12-17 21:03:59 +0800

    开发过渡动画组件

---
+++ ImoocMall/src/common/fade/Fade.vue
@@ -0,0 +1,16 @@
+<template>
+    <transition>
+        <slot></slot>
+    </transition>
+</template>
+
+<script>
+export default {
+    name : 'Fade'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
