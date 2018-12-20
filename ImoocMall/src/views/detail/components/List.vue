2a1d04b73b89e62ef821cc955ab198eaf761a277
ygn <947498533@qq.com>
2018-12-20 21:51:22 +0800

    vue递归组件通过判断树状数据进行递归，通过name自己使用自己

--- ImoocMall/src/views/detail/components/List.vue
+++ ImoocMall/src/views/detail/components/List.vue
@@ -9,12 +9,16 @@
                 <span class="item-title-icon"></span>
                 {{item.title}}
             </div>
+            <div v-if="item.children" class="item-children">
+                <detail-list :list="item.children"></detail-list>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    //子组件也可以自己调用自己，所以就使用到了 name 属性
     name : 'DetailList',
     props : {
         list : Array
@@ -24,19 +28,21 @@ export default {
 
 <style lang="stylus" scoped>
 .item-title-icon {
-    display: inline-block;
-    position: relative;
-    width: .36rem;
-    height: .36rem;
-    top: .06rem;
-    left: .06rem;
-    background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
-    margin-right: .1rem;
-    background-size: .4rem 3rem;
+    display: inline-block
+    position: relative
+    width: .36rem
+    height: .36rem
+    top: .06rem
+    left: .06rem
+    background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right: .1rem
+    background-size: .4rem 3rem
 }
 .item-title
     line-height 0.8rem
     font-size 0.32rem
     padding 0 0.2rem
     border-bottom 1px solid #ccc
+.item-children
+    padding 0 0.2rem
 </style>
